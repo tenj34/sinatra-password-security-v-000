edb560520183ea185f3c11f6fca4830fa2798c4a
Juan Tenezaca <tenj34@gmail.com>
2018-10-21 20:01:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,6 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
 		user = User.find_by(username: params[:username])
 	 if user && user.authenticate(params[:password])
 		 session[:user_id] = user.id
