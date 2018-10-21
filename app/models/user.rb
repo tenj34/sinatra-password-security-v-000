ce55e5883774ea0f5eba99b4f3e00b5f71103fa4
Juan Tenezaca <tenj34@gmail.com>
2018-10-21 20:04:15 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,3 @@
 class User < ActiveRecord::Base
-	def up
-		create_table :users do |t|
-			t.string :username
-			t.string :password_digest
-		end
-	end
 
-	def down
-		drop_table :users
-	end
 end
